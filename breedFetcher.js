8b3ff7ad78865fc3e848ef26ccd0334f53fcec6e
EagerBeaver98 <jooke98@outlook.com>
2020-11-06 00:24:26 +0000

    Testing and index linking

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,16 @@
 const request = require('request');
-const userInput = process.argv[2].toLowerCase().slice(0, 3);
-request("https://api.thecatapi.com/v1/breeds/search?q=" + userInput, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (error) {
-    console.log('error: ', error);
-  } else if (data[0] === undefined) {
-    console.log("Invalid breed type");
-  } else {
-    console.log(data[0].description);
-  }
-});
-  
+
+const fetchBreedDescription = (breedName, callback) => {
+  request("https://api.thecatapi.com/v1/breeds/search?q=" + breedName, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (error) {
+      callback(error, null);
+    } else if (!data[0]) {
+      callback(`Breed ${breedName} not found.`, null);
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
